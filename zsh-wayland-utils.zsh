7b14c1225c6e647e39659fadf7c668d9e215a808
Troels Bjørnskov <troels@bjoernskov.org>
2021-03-25 13:05:45 +0100

    Initial commit

---
+++ zsh-wayland-utils.zsh
@@ -0,0 +1,28 @@
+ss() {
+    if [[ $#@ -lt 1 ]]
+    then
+        colorcode=$(grim -g "$(env XCURSOR_SIZE=48 slurp -p )" -t ppm - | convert - -format "%[pixel:p{0,0}]" txt:- | awk 'FNR == 2 {print $2 " " $3}' | tr -d "\n" | tee /dev/tty | sed 's/.*#//') 2> /dev/null
+        if [ $colorcode ]
+        then
+            perl -e 'foreach $a(@ARGV){print " \e[48:2::".join(":",unpack("C*",pack("H*",$a)))."m \e[49m"; print"\e[48:2::".join(":",unpack("C*",pack("H*",$a)))."m \e[49m "};print "\n"' "$colorcode"
+            wl-copy -n $colorcode
+        fi
+    else
+        perl -e 'foreach $a(@ARGV){print " \e[48:2::".join(":",unpack("C*",pack("H*",$a)))."m \e[49m"; print"\e[48:2::".join(":",unpack("C*",pack("H*",$a)))."m \e[49m "};print "\n"' "$@"
+    fi
+}
+
+notify(){
+    if [ $? -eq 0 ]; then
+        title="Succes"
+        icon="process-completed"
+    else
+        title="Failure"
+        icon="dialog-error"
+    fi
+
+    message="$(echo $PWD | sed -e 's/\/.*\///g')"
+    [ "$1" ] && message=$1
+    swaymsg "output * dpms on"
+    notify-send "Notification $title" "$message" --icon="$icon" --expire-time=99999
+}
