dfbf66ab6dffd8d71b0807922e3d62cfb9261834
Troels Bjørnskov <troels@bjoernskov.org>
2021-03-25 13:21:45 +0100

    Crete wifi helper in fzf
    There is currently no nice frontend for iwctl. This commit creates a
    function that via fzf makes the experience a little more comfortable.

--- zsh-wayland-utils.zsh
+++ zsh-wayland-utils.zsh
@@ -87,6 +87,23 @@ bindkey -e " " sublime-go-to-file-path
 
 
 if command -v iwctl &> /dev/null &&
+
+    wifi() {
+        clear
+        ! systemctl is-active --quiet iwd && sudo systemctl enable --now --quiet iwd.service && notify-send "Wi-Fi Manager" "Turning Wi-Fi on" --icon=preferences-system-network
+        sudo /usr/bin/rfkill unblock wifi
+        iwctl station wlan0 scan on
+        clear
+        local name=$(iwctl station wlan0 get-networks | sed -e "/Available networks/d" -e "/------/d" -e "s/^\x1b\[[0-9;]*m//" -e "/^\s*$/d" -e "s/^\s...//g" -e "s/^.....>.....\(.*\)/`printf "\x1B[1m\033[3m"`\1`printf "\033[0m"`/" | fzf --color='prompt:3,header:bold:underline:8' --no-preview --bind 'change:reload(iwctl station wlan0 get-networks | sed -e "/Available networks/d" -e "/------/d" -e "s/^\x1b\[[0-9;]*m//" -e "/^\s*$/d" -e "s/^\s...//g" -e "s/^.....>.....\(.*\)/`printf "\x1B[1m\033[3m"`\1`printf "\033[0m"`/")' --bind 'tab:reload(iwctl station wlan0 get-networks | sed -e "/Available networks/d" -e "/------/d" -e "s/^\x1b\[[0-9;]*m//" -e "/^\s*$/d" -e "s/^\s...//g" -e "s/^.....>.....\(.*\)/`printf "\x1B[1m\033[3m"`\1`printf "\033[0m"`/")' --inline-info --reverse --header-lines=1 --ansi --no-multi | sed -e 's/................$//' | xargs)
+        iwctl station wlan0 scan off
+        if [[ -z ${name} ]]; then
+            return 0
+        fi
+        iwctl station wlan0 connect $name
+        wait
+        /usr/lib/systemd/systemd-networkd-wait-online --ignore=lo --timeout=30 --interface=wlan0 --operational-state=dormant && notify-send "Wi-Fi Manager" --icon=preferences-system-network "Connected to $(iw dev wlan0 link | grep -oP '(?<=SSID: ).+')" && pkill -RTMIN+13 i3blocks
+    }
+
     wifipw() {
         ! systemctl is-active --quiet iwd.service && echo "Wi-Fi service is not running" && return 1
         ssid="$(iw dev wlan0 link | grep --color=never -oP '(?<=SSID: ).+')"
