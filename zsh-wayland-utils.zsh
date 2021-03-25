4d4aa22297c8571af930b243bb517555f3ff4c34
Troels Bjørnskov <troels@bjoernskov.org>
2021-03-25 13:43:54 +0100

    Fix sublime-go-to-file-path

--- zsh-wayland-utils.zsh
+++ zsh-wayland-utils.zsh
@@ -79,7 +79,7 @@ sublime-go-to-file-path() {
     subl --command copy_filename
     read subldir </tmp/sublfile 2> /dev/null
     cd "$subldir" 2> /dev/null
-    zle xterm_title_precmd
+    xterm_title_precmd
     zle reset-prompt
 }
 zle -N sublime-go-to-file-path
