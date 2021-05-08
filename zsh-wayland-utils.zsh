545905eae88acc3f0b08f40206d91c5e47581ce2
Troels Bjørnskov <troels@bjoernskov.org>
2021-05-08 20:14:15 +0200

    Simplify color picker

--- zsh-wayland-utils.zsh
+++ zsh-wayland-utils.zsh
@@ -4,7 +4,7 @@ __colorpicker() {
         colorcode=$(grim -g "$(env XCURSOR_SIZE=48 slurp -p )" -t ppm - | convert - -format "%[pixel:p{0,0}]" txt:- | awk 'FNR == 2 {print $2 " " $3}' | tr -d "\n" | tee /dev/tty | sed 's/.*#//') 2> /dev/null
         if [ $colorcode ]
         then
-            perl -e 'foreach $a(@ARGV){print "\e[48:2::".join(":",unpack("C*",pack("H*",$a)))."m \e[49m"; print"\e[48:2::".join(":",unpack("C*",pack("H*",$a)))."m \e[49m "};print "\n"' "$colorcode"
+            perl -e 'foreach $a(@ARGV){print " \e[48:2::".join(":",unpack("C*",pack("H*",$a)))."m  \e[49m"; };print "\n"' "$colorcode"
             wl-copy -n $colorcode
         fi
     else
