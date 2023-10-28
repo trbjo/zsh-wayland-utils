2a190361919d9703689da0ca291776f60c8faa6d
Troels Bjørnskov <troels@bjoernskov.org>
2023-10-28 09:17:44 +0200

    change default wifi interface

--- zsh-wayland-utils.zsh
+++ zsh-wayland-utils.zsh
@@ -1,7 +1,3 @@
-#subwords, see alaccritty config
-bindkey "^[s" vi-backward-blank-word
-bindkey "^[t" vi-forward-blank-word
-
 # Make it easy to files for wayland session
 if [[ ! -d "${HOME}/.grconfig" ]]; then
     export INSTALL_WAYLAND_CONFIG="git clone --depth=1 --bare https://github.com/trbjo/grconfig $HOME/.grconfig &&\
@@ -56,13 +52,13 @@ then
 
     wifi() {
         ! systemctl is-active --quiet iwd && doas /usr/bin/systemctl enable --now --quiet iwd.service && notify-send "Wi-Fi Manager" "Turning Wi-Fi on" --icon=preferences-system-network
-        doas /usr/bin/ip link set dev wlan0 up
+        doas /usr/bin/ip link set dev wlan up
         doas /usr/bin/rfkill unblock wifi
-        iwctl station wlan0 scan on
+        iwctl station wlan scan on
 
         local name=$(\
-            local evalstr='iwctl station wlan0 get-networks | sed -e "/Available networks/d" -e "s/\x1b\[[0-9;]*m//g" -e "/------/d" -e "/^\s*$/d" | cut -c 7-'
-            # local evalstr='iwctl station wlan0 get-networks | sed -e "/Available networks/d" -e "/------/d" -e "s/^\x1b\[[0-9;]*m//" -e "/^\s*$/d" -e "s/^\s...//g" -e "s/^.....>.....\(.*\)/`printf "\x1B[1m\033[3m"`\1`printf "\033[0m"`/"'
+            local evalstr='iwctl station wlan get-networks | sed -e "/Available networks/d" -e "s/\x1b\[[0-9;]*m//g" -e "/------/d" -e "/^\s*$/d" | cut -c 7-'
+            # local evalstr='iwctl station wlan get-networks | sed -e "/Available networks/d" -e "/------/d" -e "s/^\x1b\[[0-9;]*m//" -e "/^\s*$/d" -e "s/^\s...//g" -e "s/^.....>.....\(.*\)/`printf "\x1B[1m\033[3m"`\1`printf "\033[0m"`/"'
             eval $evalstr | fzf --color='prompt:3,header:bold:underline:7'\
             --no-preview\
             --bind "change:reload(eval $evalstr)"\
@@ -78,17 +74,17 @@ then
         if [[ -z ${name} ]]; then
             return 0
         fi
-        iwctl station wlan0 connect "$name"
+        iwctl station wlan connect "$name"
         wait
-        /usr/lib/systemd/systemd-networkd-wait-online --ignore=lo --timeout=30 --interface=wlan0 --operational-state=dormant && notify-send "Wi-Fi Manager" --icon=preferences-system-network "Connected to $(iw dev wlan0 link | grep -oP '(?<=SSID: ).+')"
-        iwctl station wlan0 scan off
+        /usr/lib/systemd/systemd-networkd-wait-online --ignore=lo --timeout=30 --interface=wlan --operational-state=dormant && notify-send "Wi-Fi Manager" --icon=preferences-system-network "Connected to $(iw dev wlan link | grep -oP '(?<=SSID: ).+')"
+        iwctl station wlan scan off
     }
 
     wifipw() {
         local before=$EPOCHREALTIME
         ! systemctl is-active --quiet iwd.service && echo "Wi-Fi service is not running" && return 1
 
-        ssid="$(iw dev wlan0 link | grep --color=never -oP '(?<=SSID: ).+')"
+        ssid="$(iw dev wlan link | grep --color=never -oP '(?<=SSID: ).+')"
         [ -z $ssid ] && echo "Not connected to a network" && return 1
 
         # requires /etc/sudoers to have the line: tb ALL=(ALL) NOPASSWD:/usr/bin/cat /var/lib/iwd/*
